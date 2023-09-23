9338bb9fed19978c80717367fcd4c6d26abcd2b1
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-09-23 12:40:49 +0900

    👔 add invite code

--- components/provider/modal-provider.tsx
+++ components/provider/modal-provider.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react';
 
 // import { EditServerModal } from '@/components/modals/edit-server-modal';
-// import { InviteModal } from '@/components/modals/invite-modal';
+import { InviteModal } from '@/components/modals/invite-modal';
 import { CreateServerModal } from '@/components/modals/create-server-modal';
 // import { MembersModal } from '@/components/modals/members-modal';
 // import { CreateChannelModal } from '@/components/modals/create-channel-modal';
@@ -28,16 +28,16 @@ export const ModalProvider = () => {
   return (
     <>
       <CreateServerModal />
-      {/* <InviteModal />
-      <EditServerModal />
-      <MembersModal />
-      <CreateChannelModal />
-      <LeaveServerModal />
-      <DeleteServerModal />
-      <DeleteChannelModal />
-      <EditChannelModal />
-      <MessageFileModal />
-      <DeleteMessageModal /> */}
+      <InviteModal />
+      {/* <EditServerModal /> */}
+      {/* <MembersModal /> */}
+      {/* <CreateChannelModal /> */}
+      {/* <LeaveServerModal /> */}
+      {/* <DeleteServerModal /> */}
+      {/* <DeleteChannelModal /> */}
+      {/* <EditChannelModal /> */}
+      {/* <MessageFileModal /> */}
+      {/* <DeleteMessageModal /> */}
     </>
   );
 };
