6b83aa3ed55e97382c2b9e83a81fa1618b3c8584
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-09-23 20:59:39 +0900

    👔 add:server-search

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -8,10 +8,10 @@ import { currentProfile } from '@/lib/current-profile';
 import { db } from '@/lib/db';
 
 import { ServerHeader } from './server-header';
-// import { ServerSearch } from './server-search';
 // import { ServerSection } from './server-section';
 // import { ServerChannel } from './server-channel';
 // import { ServerMember } from './server-member';
+import { ServerSearch } from './server-search';
 
 interface ServerSidebarProps {
   serverId: string;
@@ -71,7 +71,7 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
   return (
     <div className='flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]'>
       <ServerHeader server={server} role={role} />
-      {/* <ScrollArea className='flex-1 px-3'>
+      <ScrollArea className='flex-1 px-3'>
         <div className='mt-2'>
           <ServerSearch
             data={[
@@ -115,7 +115,7 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
           />
         </div>
         <Separator className='bg-zinc-200 dark:bg-zinc-700 rounded-md my-2' />
-        {!!textChannels?.length && (
+        {/* {!!textChannels?.length && (
           <div className='mb-2'>
             <ServerSection sectionType='channels' channelType={ChannelType.TEXT} role={role} label='Text Channels' />
             <div className='space-y-[2px]'>
@@ -154,8 +154,8 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
               ))}
             </div>
           </div>
-        )}
-      </ScrollArea> */}
+        )} */}
+      </ScrollArea>
     </div>
   );
 };
