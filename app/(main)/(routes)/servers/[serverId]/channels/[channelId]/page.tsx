47df453b528377f57beaad4619437fbd8c38a101
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-09-24 19:07:30 +0900

    👔 add:chat message emoji

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -4,7 +4,7 @@ import { ChannelType } from '@prisma/client';
 
 import { currentProfile } from '@/lib/current-profile';
 import { ChatHeader } from '@/components/chat/chat-header';
-// import { ChatInput } from '@/components/chat/chat-input';
+import { ChatInput } from '@/components/chat/chat-input';
 // import { ChatMessages } from '@/components/chat/chat-messages';
 // import { MediaRoom } from '@/components/media-room';
 import { db } from '@/lib/db';
@@ -43,9 +43,9 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
   return (
     <div className='bg-white dark:bg-[#313338] flex flex-col h-full'>
       <ChatHeader name={channel.name} serverId={channel.serverId} type='channel' />
-      {/* {channel.type === ChannelType.TEXT && (
+      {channel.type === ChannelType.TEXT && (
         <>
-          <ChatMessages
+          {/* <ChatMessages
             member={member}
             name={channel.name}
             chatId={channel.id}
@@ -58,7 +58,7 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
             }}
             paramKey='channelId'
             paramValue={channel.id}
-          />
+          /> */}
           <ChatInput
             name={channel.name}
             type='channel'
@@ -70,7 +70,7 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
           />
         </>
       )}
-      {channel.type === ChannelType.AUDIO && <MediaRoom chatId={channel.id} video={false} audio={true} />}
+      {/* {channel.type === ChannelType.AUDIO && <MediaRoom chatId={channel.id} video={false} audio={true} />}
       {channel.type === ChannelType.VIDEO && <MediaRoom chatId={channel.id} video={true} audio={true} />} */}
     </div>
   );
