fb17af15ede93814c19821ce537a3004f94a7528
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-09-24 22:11:39 +0900

    🩹 socket

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -6,7 +6,7 @@ import { Member, Message, Profile } from '@prisma/client';
 import { Loader2, ServerCrash } from 'lucide-react';
 
 import { useChatQuery } from '@/hooks/use-chat-query';
-// import { useChatSocket } from '@/hooks/use-chat-socket';
+import { useChatSocket } from '@/hooks/use-chat-socket';
 // import { useChatScroll } from '@/hooks/use-chat-scroll';
 
 import { ChatWelcome } from './chat-welcome';
@@ -46,7 +46,7 @@ export const ChatMessages = ({ name, member, chatId, apiUrl, socketUrl, socketQu
     paramKey,
     paramValue,
   });
-  // useChatSocket({ queryKey, addKey, updateKey });
+  useChatSocket({ queryKey, addKey, updateKey });
   // useChatScroll({
   //   chatRef,
   //   bottomRef,
