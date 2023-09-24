16bcc050ac6a7b95280845564ac0971433a6d8db
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-09-24 16:09:39 +0900

    👔 add:socket.io

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { ClerkProvider } from '@clerk/nextjs';
 import { cn } from '@/lib/utils';
 import { ThemeProvider } from '@/components/provider/theme-provider';
 import { ModalProvider } from '@/components/provider/modal-provider';
+import { SocketProvider } from '@/components/provider/socket-provider';
 
 const font = Open_Sans({ subsets: ['latin'] });
 
@@ -21,8 +22,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <html lang='ja' suppressHydrationWarning>
         <body className={cn(font.className, 'bg-white dark:bg-[#313338]')}>
           <ThemeProvider attribute='class' defaultTheme='dark' enableSystem={false} storageKey='discord-theme'>
-            <ModalProvider />
-            {children}
+            <SocketProvider>
+              <ModalProvider />
+              {children}
+            </SocketProvider>
           </ThemeProvider>
         </body>
       </html>
--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,7 +1,7 @@
 import { Hash } from 'lucide-react';
 
   import { UserAvatar } from '@/components/user-avatar';
-// import { SocketIndicator } from '@/components/socket-indicator';
+import { SocketIndicator } from '@/components/socket-indicator';
 
 // import { ChatVideoButton } from './chat-video-button';
 import { MobileToggle } from '../mobile-toggle';
@@ -20,10 +20,10 @@ export const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) 
       {type === 'channel' && <Hash className='w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2' />}
       {type === 'conversation' && <UserAvatar src={imageUrl} className='h-8 w-8 md:h-8 md:w-8 mr-2' />}
       <p className='font-semibold text-md text-black dark:text-white'>{name}</p>
-      {/* <div className='ml-auto flex items-center'>
-        {type === 'conversation' && <ChatVideoButton />}
+      <div className='ml-auto flex items-center'>
+        {/* {type === 'conversation' && <ChatVideoButton />} */}
         <SocketIndicator />
-      </div> */}
+      </div>
     </div>
   );
 };
